ec240b5a64f81b1acb0c60d10b690d7fea1a8bc6
Ramazan Sakin <ramazansakin63@gmail.com>
2020-02-15 10:58:33 +0300

    Project base via spring initializer

---
+++ src/test/java/com/sakinramazan/micros/organizationschedulingpro/OrganizationSchedulingProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakinramazan.micros.organizationschedulingpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationSchedulingProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
