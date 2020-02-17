3ad8951427699921a46be71de58b9b1af7c5de97
Ramazan Sakin <ramazansakin63@gmail.com>
2020-02-17 04:31:53 +0300

    Swagger_' api documentation added

--- src/main/java/com/sakinramazan/micros/organizationschedulingpro/OrganizationSchedulingProApplication.java
+++ src/main/java/com/sakinramazan/micros/organizationschedulingpro/OrganizationSchedulingProApplication.java
@@ -2,12 +2,14 @@ package com.sakinramazan.micros.organizationschedulingpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class OrganizationSchedulingProApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrganizationSchedulingProApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizationSchedulingProApplication.class, args);
+    }
 
 }
