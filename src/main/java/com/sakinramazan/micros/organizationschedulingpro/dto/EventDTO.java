880a452be963a6d619fa67d6d7ebe2eec522ce0a
Ramazan Sakin <ramazansakin63@gmail.com>
2020-11-10 14:19:53 +0300

    EventDTO fields jackson de/serilization obj adapted

--- src/main/java/com/sakinramazan/micros/organizationschedulingpro/dto/EventDTO.java
+++ src/main/java/com/sakinramazan/micros/organizationschedulingpro/dto/EventDTO.java
@@ -1,5 +1,6 @@
 package com.sakinramazan.micros.organizationschedulingpro.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 import java.io.Serializable;
@@ -7,9 +8,19 @@ import java.io.Serializable;
 @Value
 public class EventDTO implements Serializable {
 
-    private String subject;
-    private String time;
-    private String duration;
-    private Integer trackNo;
+    String subject;
+    String time;
+    String duration;
+    Integer trackNo;
 
+    // For proper deserilization via jackson
+    public EventDTO(@JsonProperty("subject") String subject,
+                    @JsonProperty("time") String time,
+                    @JsonProperty("duration") String duration,
+                    @JsonProperty("trackNo") Integer trackNo) {
+        this.subject = subject;
+        this.time = time;
+        this.duration = duration;
+        this.trackNo = trackNo;
+    }
 }
