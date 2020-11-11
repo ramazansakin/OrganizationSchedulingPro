19c8a4d011d364b5e3b8cdc8e4534dc0829175e8
Ramazan Sakin <ramazansakin63@gmail.com>
2020-11-11 17:15:07 +0300

    model mapper bean conf added

---
+++ src/main/java/com/sakinramazan/micros/organizationschedulingpro/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.sakinramazan.micros.organizationschedulingpro.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
--- src/main/java/com/sakinramazan/micros/organizationschedulingpro/service/impl/EventServiceImpl.java
+++ src/main/java/com/sakinramazan/micros/organizationschedulingpro/service/impl/EventServiceImpl.java
@@ -10,6 +10,7 @@ import com.sakinramazan.micros.organizationschedulingpro.service.EventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,8 +22,10 @@ public class EventServiceImpl implements EventService {
 
     private final EventRepository eventRepository;
     private final EventElasticRepository eventElasticRepository;
-    private final ModelMapper modelMapper;
     private final KafkaMessageProducer kafkaMessageProducer;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
     public List<Event> getAllEvents() {
